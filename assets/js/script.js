2e357fa466ba9ac32e3465134bb7408fdb8d9d8a
Adam Norton <adam@adamnorton.com>
2019-12-10 02:14:23 -0600

    demo complete

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var backArr = ["url(./assets/images/bg_science01.jpg)", "url(assets/images/bg_science02.jpg)", "url(assets/images/bg_science03.jpg)", "url(assets/images/bg_science04.jpg)", "url(assets/images/bg_science05.jpg)", "url(assets/images/bg_science06.jpg)"];
+
+var randomBackgroundNumber = Math.floor(Math.random() * backArr.length);
+console.log("randome number is " + randomBackgroundNumber);
+
+document.getElementById('bg-pg5').style.backgroundImage = backArr[randomBackgroundNumber];
